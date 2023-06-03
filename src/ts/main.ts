657f23ef0f9d9ff9c030fa56f57edfb9268ce37a
ukonpower <ore.ukonpower@gmail.com>
2023-06-03 12:16:23 +0900

    wip

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -11,25 +11,6 @@ class App {
 
 	constructor() {
 
-		// elms
-
-		document.body.innerHTML = `
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
-		<style>
-			body{margin:0;width:100%;height:100%;overflow:hidden;}
-			.cw{position:relative;width:100%;height:100%;background:#000;}
-			canvas{width:100%;height:100%;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);}
-			h1,h2{position:absolute;font-family:'Bebas Neue',sans-serif;}
-			h1{left:1%;top:7%;font-size:20vmin;}
-			h2{right:1vmin;bottom:-11vmin;font-size:40vmin;mix-blend-mode:difference;}
-		</style>
-		<div class="cw"></div>
-		<h1>HAKIDAME</h1>
-		<h2>1</h2>
-		`;
-
 		this.canvasWrap = document.querySelector( '.cw' )!;
 
 		this.canvas = canvas;
@@ -64,7 +45,7 @@ class App {
 
 		const canvasAspect = window.innerWidth / window.innerHeight;
 
-		const scale = canvasAspect < 1.0 ? 0.9 : 1.0;
+		const scale = canvasAspect < 1.0 ? 0.8 : 1.5;
 
 		const blkRatioX = canvasAspect < 1.0 ? 0.75 : 1.0;
 		const blkRatioY = canvasAspect < 1.0 ? 0.7 : 0.5;
